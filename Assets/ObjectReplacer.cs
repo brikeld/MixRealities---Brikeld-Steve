ae802d1e3e9e4fe1805efd355ad1cfcd289cb567
brikeld <129516277+brikeld@users.noreply.github.com>
2025-02-13 15:19:28 +0100

    thx mario

--- Assets/ObjectReplacer.cs
+++ Assets/ObjectReplacer.cs
@@ -7,14 +7,42 @@ public class ObjectReplacer : MonoBehaviour
 
     public Vector3 offsetPosition;
 
+    public LayerMask layerMask = 0;
+
+    public Vector3 boxSize;
+    public Vector3 boxCenter;
+
+    float timeSinceNoInstance = 10000;
+
+    public float spawnDelay = 0.5f;
+
     void OnEnable()
     {
-        Update();
+        FixedUpdate();
     }
 
-    void Update()
+    void FixedUpdate()
     {
+
         if (!instance)
-            instance = Instantiate(prefab, transform.TransformPoint(offsetPosition), transform.rotation);
+        {
+            timeSinceNoInstance += Time.deltaTime;
+        }
+        else
+        {
+            timeSinceNoInstance = 0;
+        }
+
+
+        if (timeSinceNoInstance > spawnDelay)
+        {
+            Vector3 halfExtents = boxSize / 2;
+
+            if (!Physics.CheckBox(transform.TransformPoint(boxCenter), halfExtents, transform.rotation, layerMask))
+            {
+
+                instance = Instantiate(prefab, transform.TransformPoint(offsetPosition), transform.rotation);
+            }
+        }
     }
 }
